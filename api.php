38295a2a5aa9e6ecad60348b18bade626a7364ac
cmgpWLapTop <cmgpguerracarlos@gmail.com>
2021-05-23 16:39:08 -0300

    add db

--- api.php
+++ api.php
@@ -1,17 +1,19 @@
 <?
 include_once './lectura.php';
 
-class Api{
-    function getAll(){
+class Api
+{
+    function getAll()
+    {
         $lectura = new Lectura();
         $lecturas = array();
         // $lecturas["items"] = array();
 
         $res = $lectura->getLecturas();
 
-        if($res->rowCount()){
-            while ($row = $res->fetch(PDO::FETCH_ASSOC)){
-                $item=array(
+        if ($res->rowCount()) {
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+                $item = array(
                     "id_estacion" => $row['id_estacion'],
                     "fecha" => $row['fecha'],
                     "temperatura" => $row['temperatura'],
@@ -24,32 +26,48 @@ class Api{
                 );
                 array_push($lecturas, $item);
             }
-        
+
             echo json_encode($lecturas);
-        }else{
+        } else {
             echo json_encode(array('mensaje' => 'No hay elementos'));
         }
     }
 
-    function addLectura($lectura){
-        $id=0;$fecha="";$tem=0;$pres=0;$vel=0;$dir=0;$sol=0;$uv=0;$hum=0;
-        foreach($lectura as $ls){
-            global $id;$fecha;$tem;$pres;$vel;$dir;$sol;$uv;$hum;
-            $id= $ls['id_estacion'];
-            $fecha=$ls['fecha'];
-            $tem+=$ls['temperatura'];
-            $pres+=$ls['presion'];
-            $vel+=$ls['velocidad_viento'];
-            $dir+=$ls['direccion_viento'];
-            $sol+=$ls['radiacion_solar'];
-            $uv+=$ls['radiacion_uv'];
-            $hum+=$ls['humedad'];
+    function addLectura($lectura)
+    {
+        $id = 0;
+        $fecha = "";
+        $tem = 0;
+        $pres = 0;
+        $vel = 0;
+        $dir = 0;
+        $sol = 0;
+        $uv = 0;
+        $hum = 0;
+        foreach ($lectura as $ls) {
+            global $id;
+            $fecha;
+            $tem;
+            $pres;
+            $vel;
+            $dir;
+            $sol;
+            $uv;
+            $hum;
+            $id = $ls['id_estacion'];
+            $fecha = $ls['fecha'];
+            $tem += $ls['temperatura'];
+            $pres += $ls['presion'];
+            $vel += $ls['velocidad_viento'];
+            $dir += $ls['direccion_viento'];
+            $sol += $ls['radiacion_solar'];
+            $uv += $ls['radiacion_uv'];
+            $hum += $ls['humedad'];
         }
-        
-        
+
+
         $l = new Lectura();
 
-        return $l->setLectura( $id,$fecha,$tem/6,$pres/6,$vel/6,$dir/6,$sol/6,$uv/6,$hum/6);
+        return $l->setLectura($id, $fecha, $tem / 6, $pres / 6, $vel / 6, $dir / 6, $sol / 6, $uv / 6, $hum / 6);
     }
-
 }
